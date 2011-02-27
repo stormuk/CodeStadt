16ac24b00a3c3372f88d2e93c59ad121c022f55b
Adam Pope <adam@storm-consultancy.com>
2011-02-27 22:13:21 +0000

    Make program draw a cube.  There is a definite bug when an object and plane get close to each other - the plane is rendered instead of the object, despite the object being infront of the plane.  This could be a rounding error, or use of int instead of double somewhere.

--- Source/CodeStadt.Application/Program.cs
+++ Source/CodeStadt.Application/Program.cs
@@ -6,6 +6,7 @@
     using CodeStadt.Draw.RayTracer;
     using CodeStadt.Draw.RayTracer.Environment;
     using CodeStadt.Draw.RayTracer.Environment.Objects;
+    using CodeStadt.Draw.RayTracer.Environment.Models;
     using Driven.Metrics.metrics;
     using Driven.Metrics.Reporting;
     using System.Collections.Generic;
@@ -75,24 +76,25 @@
             var screen = new Screen(width, height);
             var MyScene = new Scene()
             {
-                Elements = new SceneObject[] { 
-                                //new Plane() {
-                                //    Norm = new Vector(0,1,0),
-                                //    Point = new Vector(0,-0.5,0),
-                                //    Surface = Surfaces.CheckerBoard
-                                //},
-                                new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,1,0), new Vector(1,1,0), new Vector(1,0,0)}, new Vector(0,0,1))
+                Elements = new List<SceneObject>() { 
+                                new Plane()
                                 {
-                                    Surface = Surfaces.White
+                                    Norm = new Vector(0, 1, 0),
+                                    Point = new Vector(0, -0.5, 0),
+                                    Surface = Surfaces.CheckerBoard
                                 }
-                                ,new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,1,0), new Vector(0,1,1), new Vector(0,0,1)}, new Vector(1,0,0))
-                                {
-                                    Surface = Surfaces.White
-                                }
-                                ,new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,0,1), new Vector(1,0,1), new Vector(1,0,0)}, new Vector(0,1,0))
-                                {
-                                    Surface = Surfaces.White
-                                }
+                                //,new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,1,0), new Vector(1,1,0), new Vector(1,0,0)}, new Vector(0,0,1))
+                                //{
+                                //    Surface = Surfaces.White
+                                //}
+                                //,new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,1,0), new Vector(0,1,1), new Vector(0,0,1)}, new Vector(1,0,0))
+                                //{
+                                //    Surface = Surfaces.White
+                                //}
+                                //,new Polygon(new List<Vector>(){ new Vector(0,0,0), new Vector(0,0,1), new Vector(1,0,1), new Vector(1,0,0)}, new Vector(0,1,0))
+                                //{
+                                //    Surface = Surfaces.White
+                                //}
                                 //,new Sphere() {
                                 //    Center = new Vector(0,1,0),
                                 //    Radius = 1,
@@ -128,26 +130,32 @@
 
                 
                 },
-                Lights = new Light[] { 
+                Lights = new List<Light>() { 
+                                //new Light() {
+                                //    Position = new Vector(-2,2.5,0),
+                                //    Color = new Color(.49,.07,.07)
+                                //},
+                                //new Light() {
+                                //    Position = new Vector(1.5,2.5,1.5),
+                                //    Color = new Color(.07,.07,.49)
+                                //},
+                                //new Light() {
+                                //    Position = new Vector(1.5,2.5,-1.5),
+                                //    Color = new Color(.07,.49,.071)
+                                //},
+                                //new Light() {
+                                //    Position = new Vector(8,5,-6),
+                                //    Color = new Color(1,0,0)
+                                //},
                                 new Light() {
-                                    Position = new Vector(-2,2.5,0),
-                                    Color = new Color(.49,.07,.07)
-                                },
-                                new Light() {
-                                    Position = new Vector(1.5,2.5,1.5),
-                                    Color = new Color(.07,.07,.49)
-                                },
-                                new Light() {
-                                    Position = new Vector(1.5,2.5,-1.5),
-                                    Color = new Color(.07,.49,.071)
-                                },
-                                new Light() {
-                                    Position = new Vector(0,3.5,0),
-                                    Color = new Color(.21,.21,.35)
+                                    Position = new Vector(8,4,8),
+                                    Color = new Color(1,1,1)
                                 }},
-                Camera = new Camera(new Vector(8, 2,6), new Vector(-1, 0, 0), screen)
+                Camera = new Camera(new Vector(6, 3, 6), new Vector(0, 0, 0), screen)
                 //Camera = new Camera(new Vector(-3, 2, -4), new Vector(-1,0.5,0))
             };
+
+            MyScene.AddModel(new Cube(1, new Vector(0, 0, 0)));
 
             rayTracer.Render(MyScene);
 
